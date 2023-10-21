e3c3d3ef229ff9949efab8ae5c9c28d3c985ea29
HarryCaveMan <harris.joseph.088@gmail.com>
2023-10-21 15:22:36 -0400

    cleaning up

--- service.py
+++ service.py
@@ -21,20 +21,19 @@ class EncoderService(metaclass=Singleton):
     def __init__(self,model_path: str) -> None:
         self.model = AutoModel.from_pretrained(model_path)
         self.tokenizer = AutoTokenizer.from_pretrained(model_path)
-
     #Mean Pooling and normalization - Take attention mask into account for correct averaging
     def get_normalized_sentence_embeddings(self,token_embeddings,attention_mask):
         input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
         embeddings = torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1),min=1e-9)
-        return F.normalize(embeddings, p=2, dim=1)
-    
+        return F.normalize(embeddings, p=2, dim=1).tolist()
+    #Get embedding vectors for list of sentences
     def encode(self,sentences:List[str]) -> List[float]:
         encoded_input = self.tokenizer(sentences,padding=True,truncation=True,return_tensors="pt")
         # Compute token embeddings
         with torch.no_grad():
             token_embeddings = self.model(**encoded_input)[0]
         # call pooling/norm method and return the normalized sentence embeddings
-        return self.get_normalized_sentence_embeddings(token_embeddings,encoded_input["attention_mask"]).tolist()
+        return self.get_normalized_sentence_embeddings(token_embeddings,encoded_input["attention_mask"])
 
 def handler(event: Dict[str,Any],context: Any) -> Dict[str,Union[int,List[str]]]:
     req_json: Dict[str,Union[int,List[str]]] = from_json(event["body"])
